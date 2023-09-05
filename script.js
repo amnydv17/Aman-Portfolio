8cb73c56e4038df5c02cc00815b984c1e803b5a1
Aman Yadav <111141971+amnydv17@users.noreply.github.com>
2023-09-06 04:30:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/*               Typing Animation          */
+var typed=new Typed(".typing",{
+    strings:["","Front-End Developer","Web Designer","Freelancer","Software Engineer","Sportsman"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
