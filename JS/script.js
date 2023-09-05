adddfdf436baa796eaf6f1da39ef244f4541c3d3
Aman Yadav <111141971+amnydv17@users.noreply.github.com>
2023-09-06 04:30:46 +0530

    Delete JS directory

--- JS/script.js
+++
@@ -1,7 +0,0 @@
-/*               Typing Animation          */
-var typed=new Typed(".typing",{
-    strings:["","Front-End Developer","Web Designer","Freelancer","Software Engineer","Sportsman"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
